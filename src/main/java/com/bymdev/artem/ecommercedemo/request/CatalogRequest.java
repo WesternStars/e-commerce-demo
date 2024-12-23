4a65dd5f7d45b91d7951e4a017e2d50791efb0af
Artem Puhachov <puhachov.artem@gmail.com>
2024-12-23 12:25:08 +0200

    Swagger was added

--- src/main/java/com/bymdev/artem/ecommercedemo/request/CatalogRequest.java
+++ src/main/java/com/bymdev/artem/ecommercedemo/request/CatalogRequest.java
@@ -1,6 +1,8 @@
 package com.bymdev.artem.ecommercedemo.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 
-public record CatalogRequest(@NotEmpty(message = "The name is required.") String name) {
+@Schema(description = "Catalog")
+public record CatalogRequest(@Schema(description = "Name", example = "Tool") @NotEmpty(message = "The name is required.") String name) {
 }
--- src/main/java/com/bymdev/artem/ecommercedemo/request/ProductRequest.java
+++ src/main/java/com/bymdev/artem/ecommercedemo/request/ProductRequest.java
@@ -1,17 +1,22 @@
 package com.bymdev.artem.ecommercedemo.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record ProductRequest(
+        @Schema(example = "ABC0001")
         @NotEmpty(message = "The sku is required.")
         String sku,
+        @Schema(example = "Hammer")
         @NotEmpty(message = "The name is required.")
         String name,
+        @Schema(example = "12.5")
         @NotNull(message = "The price is required.")
         @Positive(message = "The price must be greater than 0")
         Double price,
+        @Schema(example = "1")
         @NotNull(message = "The categoryId is required.")
         @Positive(message = "The categoryId must be greater than 0")
         Integer categoryId
